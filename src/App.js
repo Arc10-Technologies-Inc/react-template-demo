56471cafe7bb049d8171031a887804d9a9ecbdfd
vatsal2809 <vatsal@arc10.io>
2023-03-21 11:07:13 -0400

    React template ready.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { checkHealth } from './services/health';
 
 function App() {
+  const [url, setUrl] = useState(false)
+
+  useEffect(async() => {
+    const { response } = await checkHealth();
+    if(response?.data?.status){
+      setUrl(process.env.REACT_APP_BACKEND_URL)
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        {url && <p>Backend connected to url : {url}</p>}
       </header>
     </div>
   );
